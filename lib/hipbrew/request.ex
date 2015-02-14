9fcb2d905f119d976240b7e7a468182eb7a53cec
David Peredo <daveperedo@gmail.com>
2015-02-13 19:22:51 -0800

    Moved handlers to request.

--- lib/hipbrew/request.ex
+++ lib/hipbrew/request.ex
@@ -2,17 +2,33 @@ defmodule Hipbrew.Request do
   @api_url "https://api.hipchat.com/v2"
 
   def get(path, params \\ []) do
-    url(path, params) |> HTTPoison.get
+    url(path, params)
+      |> HTTPoison.get
+      |> handle_response
   end
 
   def post(path, params, query_params \\ []) do
     header = %{"Content-Type": "application/json"}
     body = Poison.encode!(params)
-    url(path, query_params) |> HTTPoison.post(body, header)
+    url(path, query_params)
+      |> HTTPoison.post(body, header)
+      |> handle_response
   end
 
   def url(path, query_params \\ []) do
     query = URI.encode_query(query_params)
     "#{@api_url}#{path}?#{query}"
+  end
+
+  defp handle_response({:ok, %{status_code: 204}}) do
+    :ok
+  end
+
+  defp handle_response({:ok, %{status_code: 200, body: body}}) do
+    Poison.decode(body)
+  end
+
+  defp handle_response({:error, message}) do
+    {:ok, message}
   end
 end
--- lib/hipbrew/room.ex
+++ lib/hipbrew/room.ex
@@ -4,13 +4,11 @@ defmodule Hipbrew.Room do
   def list_rooms(api_token) do
     "/room"
       |> get(%{api_token: api_token})
-      |> handle_response
   end
 
   def room_history(room_id, api_token) do
     "/room/#{room_id}/history/latest"
       |> get(%{api_token: api_token})
-      |> handle_response
   end
 
   def send_message(room_id, message, api_token) do
@@ -18,7 +16,6 @@ defmodule Hipbrew.Room do
 
     "/room/#{room_id}/notification"
       |> post(params, %{api_token: api_token})
-      |> handle_response
   end
 
   def create_webhook(room_id, url, event, api_token, options \\ []) do
@@ -26,18 +23,6 @@ defmodule Hipbrew.Room do
 
     "/room/#{room_id}/webook"
       |> post(params, %{api_token: api_token})
-      |> handle_response
   end
 
-  defp handle_response({:ok, %{status_code: 204}}) do
-    :ok
-  end
-
-  defp handle_response({:ok, %{status_code: 200, body: body}}) do
-    Poison.decode(body)
-  end
-
-  defp handle_response({:error, message}) do
-    {:ok, message}
-  end
 end
